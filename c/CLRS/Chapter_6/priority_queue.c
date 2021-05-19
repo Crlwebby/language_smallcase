005cbd8646b1529bde165b996cfccbab6fb6de97
Crlwebby <1303965943@qq.com>
2021-05-19 19:53:13 +0800

    算导，堆，总算写完了……

--- c/CLRS/Chapter_6/priority_queue.c
+++ c/CLRS/Chapter_6/priority_queue.c
@@ -1,37 +1,88 @@
 #include<stdio.h>
+#include<limits.h>
+#include<stdlib.h>
 #define PARENT(i) ((i - 1)/2)
 #define LEFT(i) 2*i + 1
 #define RIGHT(i) 2*i + 2
 
 int heap_maximum(int a[]);
+//获得队列中的最大值，但不出队
 int heap_extract_max(int a[],int *heap_size);
+//最大优先队列之出队，取出队列中的最大值
 void max_heapify(int a[], int heap_size, int i);
-void increase_key(int a[], int x, int k);
-void max_heap_insert(int a[], int key);
-int main(){
+//维持最大堆的性质
+void increase_key(int a[], int position, int k);
+//增加队列中某个元素的值，并维持优先队列
+int *max_heap_insert(int a[], int *heap_size, int key);
+//队列中新增元素
+void build_max_heap(int a[], int heap_size);
+//建堆
 
+int main(){
+    int *a;
+    int permanent[] = {3,7,9,11,18,29,4};
+    int len = sizeof(permanent)/sizeof(permanent[0]);
+    printf("%d\n",len);
+    a = permanent;
+    build_max_heap(a,len);
+    for(int i = 0; i < len; i++){
+        printf("%d\t",a[i]);
+    }
+    printf("\n");
+    printf("%d\n",heap_extract_max(a,&len));
+    for(int i = 0; i < len; i++){
+        printf("%d\t",a[i]);
+    }
+    printf("\n");
+    a = max_heap_insert(a,&len,255);
+    for(int i = 0; i < len; i++){
+        printf("%d\t",a[i]);
+    }
+    printf("\n");
+    while(len > 0){
+        printf("%d\t",heap_extract_max(a,&len));
+    }
+    printf("\n");
     return 0;
 }
 
-void increase_key(int a[], int i, int key){
-    if(a[i] >= key)
+void build_max_heap(int a[], int heap_size){
+    for(int i = heap_size/2; i >= 0; i--){
+        max_heapify(a,heap_size,i);   
+    }
+}
+
+int *max_heap_insert(int a[], int *heap_size, int key){
+    int *temp = (int *)malloc(sizeof(int) * (*heap_size + 1));
+    for(int i = 0; i < *heap_size; i++){
+        temp[i] = a[i];
+    }
+    temp[*heap_size] = INT_MIN;
+    (*heap_size)++;
+    increase_key(temp,*heap_size-1,key);
+    return temp;
+}
+
+void increase_key(int a[], int position, int key){
+    if(a[position] >= key)
         return;
-    a[i] = key;
-    while(i > 1 && a[PARENT(i)] < a[i]){
-        int temp = a[PARENT(i)];
-        a[PARENT(i)] = a[i];
-        a[i] = temp;
-        i - PARENT(i);
+    a[position] = key;
+    while(position > 0 && a[PARENT(position)] < a[position]){
+        int temp = a[PARENT(position)];
+        a[PARENT(position)] = a[position];
+        a[position] = temp;
+        position = PARENT(position);
     }
 }
 int heap_extract_max(int a[],int *heap_size){
     if(*heap_size < 1){
-        printf("heap underflow");
+        printf("heap underflow!\n");
         exit(-1);
     }
     int max = a[0];
-    a[0] = a[*heap_size - 1];
-    max_heapify(a,*heap_size - 1,0);
+    (*heap_size)--;
+    a[0] = a[*heap_size];
+    max_heapify(a,*heap_size,0);
     return max;
 }
 
---
+++ c/CLRS/Chapter_6/priority_queue_2_stack.c
@@ -0,0 +1,120 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#define PARENT(i) ((i - 1)/2)
+#define LEFT(i) 2*i + 1
+#define RIGHT(i) 2*i + 2
+int PRIORITY = INT_MIN;
+//用优先队列实现队列，多加个优先级变量应该就可以了吧……
+
+typedef struct {
+    int val;
+    int priority;
+}data;
+
+data heap_maximum(data a[]);
+data heap_extract_max(data a[], int *heap_size);
+void max_heapify(data a[], int heap_size, int position);
+void exchange(data *a, data *b);
+data *max_heap_insert(data *a, int *heap_size, data key);
+void increase_key(data a[], int position, int key);
+data *push(data *a, int *heap_size, int key);
+data pop(data a[], int *heap_size);
+
+int main(){
+    data *a = (data *)malloc(sizeof(data));
+    a->priority = PRIORITY++;
+    a->val = 20;
+    int len = 1;
+    a = push(a,&len,12);
+    a = push(a,&len,18);
+    a = push(a,&len,22);
+    while(len){
+        data temp = pop(a,&len);
+        printf("%d\t%d\n",temp.priority,temp.val);
+    }
+    return 0;
+}
+
+data pop(data a[], int *heap_size){
+    return heap_extract_max(a, heap_size);
+}
+
+data *push(data *a, int *heap_size, int key){
+    data temp;
+    temp.val = key;
+    temp.priority = PRIORITY++;
+    int len = *heap_size;
+    data *b = max_heap_insert(a,&len,temp);
+    *heap_size = len;
+    return b;
+}
+
+data heap_extract_max(data a[], int *heap_size){
+    if(*heap_size < 1){
+        printf("heap underflow!\n");
+        exit(-1);
+    }
+    data temp = a[0];
+    (*heap_size)--;
+    a[0] = a[*heap_size];
+    max_heapify(a,*heap_size,0);
+    return temp;
+}
+
+void increase_key(data a[], int position, int key){
+    if(key < a[position].priority){
+        printf("key is smaller than current value");
+        return;
+    }
+    a[position].priority = key;
+    while(position > 0 && a[PARENT(position)].priority < a[position].priority){
+        exchange(&a[PARENT(position)],&a[position]);
+        position = PARENT(position);
+    }
+}
+
+data heap_maximum(data a[]){
+    return a[0];
+}
+
+data *max_heap_insert(data *a, int *heap_size, data key){
+    data *temp = (data *)malloc(sizeof(data) * (*heap_size + 1));
+    for(int i = 0; i < *heap_size; i++){
+        temp[i].val = a[i].val;
+        temp[i].priority = a[i].priority;
+    }
+    free(a);
+    temp[*heap_size].val = key.val;
+    temp[*heap_size].priority = key.priority;
+    (*heap_size)++;
+    increase_key(temp,*heap_size-1,key.priority);
+    return temp;
+}
+
+void exchange(data *a, data *b){
+    data temp;
+    temp.val = a->val;
+    temp.priority = a->priority;
+    a->val = b->val;
+    a->priority = b->priority;
+    b->val = temp.val;
+    b->priority = temp.priority;
+
+}
+
+void max_heapify(data a[], int heap_size, int position){
+    int l = LEFT(position);
+    int r = RIGHT(position);
+    int max = position;
+    if(l < heap_size && a[l].priority > a[position].priority){
+        max = l;
+    }
+    if(r < heap_size && a[r].priority > a[max].priority){
+        max = r;
+    }
+    if(max != position){
+        exchange(&a[position],&a[max]);
+        max_heapify(a,heap_size,max);
+    }
+}
